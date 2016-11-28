81aed8e0994d8791f707325533c3c3e2b51b858b
ionyuop@gmail.com <ionyuop@gmail.com>
2016-11-28 15:18:54 +0100

    Terminado

--- ProyectoPSP/src/ProyectoPSP/Cliente.java
+++ ProyectoPSP/src/ProyectoPSP/Cliente.java
@@ -34,7 +34,7 @@ public class Cliente {
     public static void main(String[] args) throws UnknownHostException, IOException {
         int opcion = 0;
         InetAddress direccion = InetAddress.getLocalHost();
-        Socket servidor = new Socket(direccion, puerto);
+        Socket servidor = null;
         DataInputStream dis = null;
         DataOutputStream dos = null;
         String rutaServidor = System.getProperty("user.home");
@@ -42,6 +42,9 @@ public class Cliente {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("La ruta por defecto es" + rutaServidor);
         do {
+            servidor = new Socket(direccion, puerto);
+            dis = new DataInputStream(servidor.getInputStream());
+            dos = new DataOutputStream(servidor.getOutputStream());
             System.out.println("Selecciona lo que quieres hacer escribiendo el numero de la opcion:"
                     + "\n1. Listar"
                     + "\n2. Enviar"
@@ -49,7 +52,6 @@ public class Cliente {
                     + "\n4. Salir"
                     + "\nopcion: ");
             opcion = Integer.parseInt(br.readLine());
-            dos = new DataOutputStream(servidor.getOutputStream());
             dos.writeInt(opcion);
             switch (opcion) {
                 case 1:
@@ -61,77 +63,94 @@ public class Cliente {
                     }
                     break;
                 case 2:
-                    System.out.println("Ruta del archivo para enviar: ");
-                    String rutaArchivo = br.readLine();
-                    // Creamos el archivo que vamos a enviar
-                    File archivo = new File(rutaArchivo);
-                    // Obtenemos el tamaño del archivo            
-                    int tamañoArchivo = (int) archivo.length();
-                    //flujo de salida
-                    dos = new DataOutputStream(servidor.getOutputStream());
-                    System.out.println("Enviando Archivo: " + archivo.getName());
-                    // Enviamos el nombre del archivo             
-                    dos.writeUTF(archivo.getName());
-                    // Enviamos el tamaño del archivo            
-                    dos.writeInt(tamañoArchivo);
-                    // Creamos flujo de entrada para realizar la lectura del archivo en bytes
-                    FileInputStream fis = new FileInputStream(rutaArchivo);
-                    BufferedInputStream bis = new BufferedInputStream(fis);
-                    // Creamos el flujo de salida para enviar los datos del archivo en bytes
-                    BufferedOutputStream bos = new BufferedOutputStream(servidor.getOutputStream());
-                    // Creamos un array de tipo byte con el tamaño del archivo 
-                    byte[] buffer = new byte[tamañoArchivo];
-                    // Leemos el archivo y lo introducimos en el array de bytes             
-                    bis.read(buffer);
-                    // Realizamos el envio de los bytes que conforman el archivo
-                    for (int i = 0; i < buffer.length; i++) {
-                        bos.write(buffer[i]);
-                    }
-                    System.out.println("Archivo Enviado: " + archivo.getName());
-                    bis.close();
-                    bos.close();
+                    Boolean rutaExiste = false;
+                    do {
+                        System.out.println("Ruta del archivo para enviar: ");
+                        String rutaArchivo = br.readLine();
+                        // Creamos el archivo que vamos a enviar
+                        File archivo = new File(rutaArchivo);
+
+                        //comprobamos si existe el fichero
+                        if (archivo.exists()) {
+                            rutaExiste = true;
+                            // Obtenemos el tamaño del archivo            
+                            int tamañoArchivo = (int) archivo.length();
+                            //flujo de salida
+                            dos = new DataOutputStream(servidor.getOutputStream());
+                            System.out.println("Enviando Archivo: " + archivo.getName());
+                            // Enviamos el nombre del archivo             
+                            dos.writeUTF(archivo.getName());
+                            // Enviamos el tamaño del archivo            
+                            dos.writeInt(tamañoArchivo);
+                            // Creamos flujo de entrada para realizar la lectura del archivo en bytes
+                            FileInputStream fis = new FileInputStream(rutaArchivo);
+                            BufferedInputStream bis = new BufferedInputStream(fis);
+                            // Creamos el flujo de salida para enviar los datos del archivo en bytes
+                            BufferedOutputStream bos = new BufferedOutputStream(servidor.getOutputStream());
+                            // Creamos un array de tipo byte con el tamaño del archivo 
+                            byte[] buffer = new byte[tamañoArchivo];
+                            // Leemos el archivo y lo introducimos en el array de bytes             
+                            bis.read(buffer);
+                            // Realizamos el envio de los bytes que conforman el archivo
+                            for (int i = 0; i < buffer.length; i++) {
+                                bos.write(buffer[i]);
+                            }
+                            System.out.println("Archivo Enviado: " + archivo.getName());
+                            bis.close();
+                            bos.close();
+                        } else {
+                            System.out.println("No existe la ruta indicada");
+                        }
+                    } while (!rutaExiste);
+
                     break;
                 case 3:
+                    //Comprobamos que el archivo existe
                     System.out.println("Nombre del archivo que quieres descargar: ");
                     String nombreArchivo = br.readLine();
+
                     System.out.println("Ruta donde quieres guardarlo: ");
                     String rutaGuardado = br.readLine();
-                    
+
                     dos = new DataOutputStream(servidor.getOutputStream());
                     dos.writeUTF(nombreArchivo);
+                    int existe = dis.readInt();
+                    if (existe == 1) {
+                        // Creamos flujo de entrada para leer los datos que envia el cliente
 
-                    // Creamos flujo de entrada para leer los datos que envia el cliente
-                    dis = new DataInputStream(servidor.getInputStream());
-                    // Obtenemos el tamaño del archivo               
-                    int tam = dis.readInt();
-                    System.out.println("Recibiendo archivo " + nombreArchivo);
-                    // Creamos flujo de salida, este flujo nos sirve para                
-                    // indicar donde guardaremos el archivo               
-                    FileOutputStream fos = new FileOutputStream(rutaGuardado + "/" + nombreArchivo);
-                    //Ruta donde se va a guardar el archivo
-                    BufferedOutputStream out = new BufferedOutputStream(fos);
-                    BufferedInputStream in = new BufferedInputStream(servidor.getInputStream());
-                    // Creamos el array de bytes para leer los datos del archivo
-                    byte[] buff = new byte[tam];
-                    // Obtenemos el archivo mediante la lectura de bytes enviados
-                    for (int i = 0; i < buff.length; i++) {
-                        buff[i] = (byte) in.read();
+                        // Obtenemos el tamaño del archivo               
+                        int tam = dis.readInt();
+                        System.out.println("Recibiendo archivo " + nombreArchivo);
+                        // Creamos flujo de salida, este flujo nos sirve para                
+                        // indicar donde guardaremos el archivo               
+                        FileOutputStream fos = new FileOutputStream(rutaGuardado + "/" + nombreArchivo);
+                        //Ruta donde se va a guardar el archivo
+                        BufferedOutputStream out = new BufferedOutputStream(fos);
+                        BufferedInputStream in = new BufferedInputStream(servidor.getInputStream());
+                        // Creamos el array de bytes para leer los datos del archivo
+                        byte[] buff = new byte[tam];
+                        // Obtenemos el archivo mediante la lectura de bytes enviados
+                        for (int i = 0; i < buff.length; i++) {
+                            buff[i] = (byte) in.read();
+                        }
+                        // Escribimos el archivo                
+                        out.write(buff);
+                        // Cerramos flujos               
+                        out.flush();
+                        in.close();
+                        out.close();
                     }
-                    // Escribimos el archivo                
-                    out.write(buff);
-                    // Cerramos flujos               
-                    out.flush();
-                    in.close();
-                    out.close();
+
                     break;
                 default:
                     break;
             }
-        } while (!"4".equals(opcion));
-
         servidor.close();
         dis.close();
         dos.close();
+        } while (!"4".equals(opcion));
+
+        
 
     }
 
--- ProyectoPSP/src/ProyectoPSP/Servidor.java
+++ ProyectoPSP/src/ProyectoPSP/Servidor.java
@@ -39,7 +39,6 @@ public class Servidor implements Runnable {
         File[] listaFicheros = rutaDestino.listFiles();
         // Enviamos al cliente la cantidad de ficheros que tenemos
         try {
-            dos = new DataOutputStream(cliente.getOutputStream());
             dos.writeInt(listaFicheros.length);
             // Le enviamos la lista
             for (int i = 0; i < listaFicheros.length; i++) {
@@ -51,55 +50,56 @@ public class Servidor implements Runnable {
 
     }
 
-    public void enviar(DataOutputStream dos, DataInputStream dis) throws IOException {
-        //Recibimos el nombre del archivo
-        dis = new DataInputStream(cliente.getInputStream());
+    public void enviar(DataOutputStream dos, DataInputStream dis, BufferedInputStream bis, BufferedOutputStream bos) throws IOException {
+        //Streams
+
+//Recibimos nombre del archivo
         String nombreArchivo = dis.readUTF();
         String rutaArchivo = rutaServidor + "/" + nombreArchivo;
-        // Creamos el archivo que vamos a enviar
+        //Comprobamos que existe
         File archivo = new File(rutaArchivo);
-        // Obtenemos el tamaño del archivo            
-        int tamañoArchivo = (int) archivo.length();
-        //flujo de salida
-        dos = new DataOutputStream(cliente.getOutputStream());
-        System.out.println("Enviando Archivo: " + archivo.getName());
-        
-        // Enviamos el tamaño del archivo            
-        dos.writeInt(tamañoArchivo);
-        // Creamos flujo de entrada para realizar la lectura del archivo en bytes
-        FileInputStream fis = new FileInputStream(rutaArchivo);
-        BufferedInputStream bis = new BufferedInputStream(fis);
-        // Creamos el flujo de salida para enviar los datos del archivo en bytes
-        BufferedOutputStream bos = new BufferedOutputStream(cliente.getOutputStream());
-        // Creamos un array de tipo byte con el tamaño del archivo 
-        byte[] buffer = new byte[tamañoArchivo];
-        // Leemos el archivo y lo introducimos en el array de bytes             
-        bis.read(buffer);
-        // Realizamos el envio de los bytes que conforman el archivo
-        for (int i = 0; i < buffer.length; i++) {
-            bos.write(buffer[i]);
+        if (archivo.exists()) {
+            dos.writeInt(1);
+            // Obtenemos el tamaño del archivo            
+            int tamañoArchivo = (int) archivo.length();
+            //flujo de salida
+            System.out.println("Enviando Archivo: " + archivo.getName());
+
+            // Enviamos el tamaño del archivo            
+            dos.writeInt(tamañoArchivo);
+            // Creamos flujo de entrada para realizar la lectura del archivo en bytes
+            FileInputStream fis = new FileInputStream(rutaArchivo);
+            bis = new BufferedInputStream(fis);
+            // Creamos el flujo de salida para enviar los datos del archivo en bytes
+            bos = new BufferedOutputStream(cliente.getOutputStream());
+            // Creamos un array de tipo byte con el tamaño del archivo 
+            byte[] buffer = new byte[tamañoArchivo];
+            // Leemos el archivo y lo introducimos en el array de bytes             
+            bis.read(buffer);
+            // Realizamos el envio de los bytes que conforman el archivo
+            for (int i = 0; i < buffer.length; i++) {
+                bos.write(buffer[i]);
+            }
+            System.out.println("Archivo Enviado: " + archivo.getName());
+        } else {
+            dos.writeInt(0);
+
         }
-        System.out.println("Archivo Enviado: " + archivo.getName());
-        bis.close();
-        bos.close();
 
     }
 
-    public void recibir(DataInputStream dis) throws IOException {
+    public void recibir(DataInputStream dis, BufferedOutputStream out, BufferedInputStream in) throws IOException {
 
-        // Creamos flujo de entrada para leer los datos que envia el cliente
-        dis = new DataInputStream(cliente.getInputStream());
         // Obtenemos el nombre del archivo               
         String nombreArchivo = dis.readUTF().toString();
         // Obtenemos el tamaño del archivo               
         int tam = dis.readInt();
         System.out.println("Recibiendo archivo " + nombreArchivo);
-        // Creamos flujo de salida, este flujo nos sirve para                
         // indicar donde guardaremos el archivo               
         FileOutputStream fos = new FileOutputStream(rutaServidor + "/" + nombreArchivo);
         //Ruta donde se va a guardar el archivo
-        BufferedOutputStream out = new BufferedOutputStream(fos);
-        BufferedInputStream in = new BufferedInputStream(cliente.getInputStream());
+        out = new BufferedOutputStream(fos);
+        in = new BufferedInputStream(cliente.getInputStream());
         // Creamos el array de bytes para leer los datos del archivo
         byte[] buffer = new byte[tam];
         // Obtenemos el archivo mediante la lectura de bytes enviados
@@ -109,49 +109,52 @@ public class Servidor implements Runnable {
 
         // Escribimos el archivo                
         out.write(buffer);
-        // Cerramos flujos               
-        in.close();
-        out.close();
     }
 
     @Override
     public void run() {
-        DataOutputStream dos = null;
-        DataInputStream dis = null;
-        BufferedOutputStream bos = null;
-        BufferedInputStream bis = null;
-        int opcion = 0;
-        //Recibe un int con la opcion
-        do {
-            try {
-                dis = new DataInputStream(cliente.getInputStream());
-                opcion = dis.readInt();
-                switch (opcion) {
-                    case 1:
-                        listar(dos);
-                        break;
-                    case 2:
-                        recibir(dis);
-                        break;
-                    case 3:
-                        enviar(dos, dis);
-                        break;
-                    default:
-                        throw new AssertionError();
+       
+            DataOutputStream dos = null;
+            DataInputStream dis = null;
+            BufferedOutputStream bos = null;
+            BufferedInputStream bis = null;
+            int opcion = 0;
+            //Recibe un int con la opcion
+            do {
+                try {
+                    dis = new DataInputStream(cliente.getInputStream());
+                    dos = new DataOutputStream(cliente.getOutputStream());
+                    opcion = dis.readInt();
+                    switch (opcion) {
+                        case 1:
+                            listar(dos);
+                            break;
+                        case 2:
+                            recibir(dis, bos, bis);
+                            break;
+                        case 3:
+                            enviar(dos, dis, bis, bos);
+                            break;
+                        default:
+                            throw new AssertionError();
+                    }
+                } catch (IOException ex) {
+                    Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
                 }
+
+            } while (!"4".equals(opcion));
+            try {
+                //Cerramos flujos
+                cliente.close();
+                dis.close();
+                dos.close();
+                bis.close();
+                bos.close();
             } catch (IOException ex) {
                 Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
             }
 
-        } while (!"4".equals(opcion));
-        try {
-            cliente.close();
-            dis.close();
-            dos.close();
-        } catch (IOException ex) {
-            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
 
-}
